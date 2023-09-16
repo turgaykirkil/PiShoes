06429b7516afee1fc9949ac79b1225db48536250
turgaykirkil <turgaykirkil@gmail.com>
2023-09-16 22:43:29 +0300

    first commit

--- Odev3/ViewController.swift
+++ Odev3/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "piShoes"
+        
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "yaziRenk1")!,                                 .font: UIFont(name: "CroissantOne-Regular", size: 22)!]
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
 
 
